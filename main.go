b206b04b8b49cd48813db6133a714185e8175b78
pintard <donovanpintard@gmail.com>
2024-05-11 01:30:40 -0600

    made small api changes

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"net/http"
 	"os"
 	"prexel-post-api/db"
-	. "prexel-post-api/handler"
-	. "prexel-post-api/utils"
+	handler "prexel-post-api/handler"
+	utils "prexel-post-api/utils"
 
 	"github.com/gorilla/mux"
 )
 
 var (
-	host     = GetEnv("DB_HOST", "localhost")
-	port     = GetEnv("DB_PORT", "5432")
-	dbname   = GetEnv("DB_NAME", "prexelpostdb")
-	user     = GetEnv("DB_USER", "prexeluser")
-	password = GetEnv("DB_PASSWORD", "password")
+	host     = utils.GetEnv("DB_HOST", "localhost")
+	port     = utils.GetEnv("DB_PORT", "5432")
+	user     = utils.GetEnv("DB_USER", "prexeluser")
+	password = utils.GetEnv("DB_PASSWORD", "password")
+	dbname   = utils.GetEnv("DB_NAME", "prexelpostdb")
 )
 
-var log *Logger = GetLoggerInstance()
+var log *utils.Logger = utils.GetLoggerInstance()
 
 func main() {
 	var err error = db.Connect(host, port, user, password, dbname)
@@ -31,9 +31,9 @@ func main() {
 	defer db.DB.Close()
 
 	var router *mux.Router = mux.NewRouter()
-	router.HandleFunc("/posts", CreatePostHandler).Methods("POST")
-	router.HandleFunc("/posts", GetPostsHandler).Methods("GET")
-	router.HandleFunc("/posts/{id:[0-9]+}", GetPostHandler).Methods("GET")
+	router.HandleFunc("/posts", handler.CreatePostHandler).Methods("POST")
+	router.HandleFunc("/posts", handler.GetPostsHandler).Methods("GET")
+	router.HandleFunc("/posts/{id:[0-9]+}", handler.GetPostHandler).Methods("GET")
 
 	serverPort := ":8080"
 	log.Info("Server is starting and listening on port " + serverPort)
--- model/prexel_post.go
+++ model/prexel_post.go
@@ -6,5 +6,8 @@ type PrexelPost struct {
 	ID     int64     `json:"id"`
 	UserId int64     `json:"user_id" binding:"required"`
 	Code   string    `json:"code" binding:"required"`
+	Title  string    `json:"title" binding:"required"`
+	Tags   []string  `json:"tags"`
+	Image  string    `json:"image"`
 	Date   time.Time `json:"date"`
 }
